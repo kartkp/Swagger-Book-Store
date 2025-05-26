d6ba9ae5b872194a45e65cbb2167452a06f1b25f
Kartikey Pandey <122464491+kartkp@users.noreply.github.com>
2025-05-26 10:58:02 +0530

    Update app.js

--- books_serv/app.js
+++ books_serv/app.js
@@ -13,12 +13,12 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('MongoDB connected'))
-.catch(err => {
-  console.error('MongoDB connection error:', err);
-  process.exit(1);
-});
-
+}).then(() => {
+    consol.log('Mongodb is connected succesfully')
+}).catch((errr) => {
+    console.error('Mongodb connection faild:', errr)
+    process.exit(1)
+})
 
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
